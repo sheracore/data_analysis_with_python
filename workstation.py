9e394d5e0335c238d2b85006601e908ec970a73b
lion7472 <m.ghaffari662@gmail.com>
2021-03-14 22:08:56 +0330

    Add pic

--- workstation.py
+++ workstation.py
@@ -8,3 +8,14 @@ print(df.columns)
 df = pd.read_csv("csv_sample.csv")
 print(df.columns)
 df.to_csv("output.csv")
+#--------------------------------------
+
+print(df.dtypes) # ==> type of all columns
+print(df.describe()) # ==> returns a statistical summary
+print(df.describe(include="all")) # ==> returns full statistical summary
+#Some of this data is NaN its because that particular statistical metric cannot be calculated for that specific
+print(df.info()) # ==> provides a concise summary of you DataFrame
+df["PAGING_INTENSITY(PPS/CELL)"] = df["PAGING_INTENSITY(PPS/CELL)"].astype("float")
+
+# To normalization between 0 and 1
+df["PAGING_INTENSITY(PPS/CELL)"] = df["PAGING_INTENSITY(PPS/CELL)"]/df["PAGING_INTENSITY(PPS/CELL)"].max()
