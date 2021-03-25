fa7b27b00e34697b766aef4aa86ccf93b0266379
lion7472 <m.ghaffari662@gmail.com>
2021-03-25 14:46:51 +0430

    Add pic

--- workstation.py
+++ workstation.py
@@ -18,7 +18,18 @@ print(df.info()) # ==> provides a concise summary of you DataFrame
 df["PAGING_INTENSITY(PPS/CELL)"] = df["PAGING_INTENSITY(PPS/CELL)"].astype("float")
 
 # To normalization between 0 and 1
-df["PAGING_INTENSITY(PPS/CELL)"] = df["PAGING_INTENSITY(PPS/CELL)"]/df["PAGING_INTENSITY(PPS/CELL)"].max()
-df["PAGING_INTENSITY(PPS/CELL)"] = (df["PAGING_INTENSITY(PPS/CELL)"]-df["PAGING_INTENSITY(PPS/CELL)"].min())/(df["PAGING_INTENSITY(PPS/CELL)"].max()-df["PAGING_INTENSITY(PPS/CELL)"].min())
+#df["PAGING_INTENSITY(PPS/CELL)"] = df["PAGING_INTENSITY(PPS/CELL)"]/df["PAGING_INTENSITY(PPS/CELL)"].max()
+#df["PAGING_INTENSITY(PPS/CELL)"] = (df["PAGING_INTENSITY(PPS/CELL)"]-df["PAGING_INTENSITY(PPS/CELL)"].min())/(df["PAGING_INTENSITY(PPS/CELL)"].max()-df["PAGING_INTENSITY(PPS/CELL)"].min())
 
-df["PAGING_INTENSITY(PPS/CELL)"] = (df["PAGING_INTENSITY(PPS/CELL)"]-df["PAGING_INTENSITY(PPS/CELL)"].mean())/df["PAGING_INTENSITY(PPS/CELL)"].std()
+#df["PAGING_INTENSITY(PPS/CELL)"] = (df["PAGING_INTENSITY(PPS/CELL)"]-df["PAGING_INTENSITY(PPS/CELL)"].mean())/df["PAGING_INTENSITY(PPS/CELL)"].std()
+
+binwidth = int(((int(max(df["RAB_EST_SR_CS"]))-int(min(df["RAB_EST_SR_CS"])))/4))
+bins = range(int(min(df["RAB_EST_SR_CS"])), int(max(df["RAB_EST_SR_CS"])) , binwidth)
+print(binwidth,bins)
+group_names = ['Low', 'Lo_Med' ,'Medium', 'Med_to_Hight' , 'High']
+df['range'] = pd.cut(df['RAB_EST_SR_CS'], bins, labels=group_names)
+print(df['RAB_EST_SR_CS'], df['range'] )
+
+print(bins)
+
+print(pd.get_dummies(df['REGION']))
